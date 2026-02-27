e44e9ba0b4cdee46678359ae58b5516f398bc7af
Paras Limbu <paras078@academiacollege.edu.np>
2026-02-27 19:47:39 +0545

    initialize reposotory

---
+++ Backend/src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
+app.use(express.json(
+    {
+        limit: '10mb',
+    }
+))
+app.use(express.urlencoded({
+    extended: true,
+    limit: '10mb',
+}))
+app.use(express.static('public'))
+app.use(cookieParser())
+
+//route import
+
+//route declaration
+
+export default app;
