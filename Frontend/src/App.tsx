0595f469e7d3218c688658f1af19e1395b50d564
Paras Limbu <paras078@academiacollege.edu.np>
2026-02-28 21:06:52 +0545

    feat: update routing in App component to directly render Home page and wrap App in BrowserRouter

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import {Navigate, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/home.page.tsx";
+
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home"/>}/>
-      <Route path="/home" element={<div>home</div>}/>
+      <Route path="/" element={<Home/>}/>
     </Routes>
   )
 }
