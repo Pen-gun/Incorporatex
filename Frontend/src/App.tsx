4703b0eff3c00d32fb98d1c8a55c9aab47e49620
Paras Limbu <paras078@academiacollege.edu.np>
2026-03-01 19:03:06 +0545

    feat: add routes for company and shareholder registration pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home.page.tsx';
+import CompanyRegistrationPage from './pages/companyRegistration.page.tsx';
+import ShareholderRegistrationPage from './pages/shareholderRegistration.page.tsx';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/company" element={<CompanyRegistrationPage />} />
+      <Route path="/shareholder" element={<ShareholderRegistrationPage />} />
     </Routes>
   );
 }
